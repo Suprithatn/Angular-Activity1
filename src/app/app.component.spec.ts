574cbe485c9addab528f7ba0bb157089cf4ed25e
Supritha <suprithatn92@gmail.com>
2021-05-11 16:20:18 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-activity1'`, () => {
+  it(`should have as title 'activity1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-activity1');
+    expect(app.title).toEqual('activity1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-activity1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('activity1 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-activity1';
+  title = 'activity1';
 }
